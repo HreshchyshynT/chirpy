a1c28f4bef60264bef56e7a4403d54f673b63e92
HreshchyshynT <hreshchyshyn.t@gmail.com>
2026-02-17 22:15:40 +0200

    add health endpoint, move fileserver path to app

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -9,11 +10,20 @@ func main() {
 
 	var root http.Dir
 
-	serveMux.Handle("/", http.FileServer(root))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(root)))
+	serveMux.HandleFunc("/healthz", checkHealth)
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
+}
+
+func checkHealth(rw http.ResponseWriter, r *http.Request) {
+	var builder strings.Builder
+	builder.WriteString("ContentType: text/plain; charset=utf-8")
+	rw.Header().Write(&builder)
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("OK"))
 }
