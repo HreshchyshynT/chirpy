f28380472ff28ff57af47eeba6620422d8a0ad2e
HreshchyshynT <hreshchyshyn.t@gmail.com>
2026-02-18 11:35:14 +0200

    add metrics and reset endpoints

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ac.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	serveMux := http.NewServeMux()
 
 	var root http.Dir
+	var config apiConfig
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(root)))
+	serveMux.Handle(
+		"/app/",
+		http.StripPrefix("/app", config.middlewareMetricsInc(http.FileServer(root))),
+	)
 	serveMux.HandleFunc("/healthz", checkHealth)
+	serveMux.HandleFunc("/metrics", config.handleMetrics)
+	serveMux.HandleFunc("/reset", config.handleReset)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -20,10 +39,23 @@ func main() {
 	server.ListenAndServe()
 }
 
-func checkHealth(rw http.ResponseWriter, r *http.Request) {
+func checkHealth(w http.ResponseWriter, r *http.Request) {
 	var builder strings.Builder
 	builder.WriteString("ContentType: text/plain; charset=utf-8")
-	rw.Header().Write(&builder)
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("OK"))
+	w.Header().Write(&builder)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (ac *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	var builder strings.Builder
+	builder.WriteString("ContentType: text/plain; charset=utf-8")
+	w.Header().Write(&builder)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %v", ac.fileserverHits.Load())
+}
+
+func (ac *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	ac.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
