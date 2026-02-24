872f387f72beb7b5ce8a8eeb12a82117e492c3cc
HreshchyshynT <hreshchyshyn.t@gmail.com>
2026-02-24 11:05:44 +0200

    change code structure

--- main.go
+++ main.go
@@ -2,29 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"sync/atomic"
 
 	"github.com/hreshchyshynt/chirpy/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-	queries        *database.Queries
-}
-
-func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ac.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	godotenv.Load()
@@ -63,23 +49,4 @@ func checkHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Write(&builder)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-}
-
-func (ac *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	var builder strings.Builder
-	template := `<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>`
-	builder.WriteString("ContentType: text/html")
-	w.Header().Write(&builder)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, template, ac.fileserverHits.Load())
-}
-
-func (ac *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
-	ac.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
 }
